538056d4357aac293f659d67fc68aef32c9033b4
Edwin Mercedes <81087505+frostyFozz@users.noreply.github.com>
2022-07-13 23:44:59 -0400

    minical2.0

--- minical.py
+++ minical.py
@@ -1,31 +1,53 @@
-from tkinter import N
 
+def suma(num1, num2):
+    return num1 + num2
+def resta(num1, num2):
+    return num1 - num2
+def multi(num1, num2):
+    return num1 * num2
+def divicion(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0 !!")
+
+def entero(num1, num2):
+    try:
+        return num1 // num2
+    except ZeroDivisionError:
+        print("Las diviciones entre 0 no estan disponibles")
 
 num1 = input("Ingresa un el numero para la operacion: ")
+num2 = input('Ingresa tu segundo numero: ')
+operador = input("Ingresa el simbolo para la operacoin: ")
+
+
 try:
     num1 = int(num1)
 except:
-    num1 = 'hola'
+    num1 = ("hola")
     exit()
-
-num2 = input('Ingresa tu segundo numero: ')
 try:
     num2 = int(num2)
-except: 
-    num2 = 'Edwin'
+   
+except:
+    num2 = ("edwin")
     exit()
+ 
 
-operador = input("Ingresa el simbolo para la operacoin: ")
-if operador == "+" :
-    print( "el resultado es", num1 + num2 )
-elif operador == "-" :
-    print( "el resultado es", num1 - num2 )
-elif operador == "*" :
-    print("el resultado es ", num1 * num2)
-elif operador == "/" :
-    print("El resultado es ", num1 / num2)
+if operador == "suma" :
+    print(suma(num1, num2 ))
+elif operador == "resta" :
+    print(resta(num1, num2 ))
+elif operador == "multi" :
+    print(multi(num1, num2))
+elif operador == "divicion" :
+    print(divicion(num1, num2))
+elif operador == "entero" :
+    print(entero(num1, num2))
+
+    
 else:
     print("No has introducido los valores correctos")
-    exit()
 
 print("\n¡Eso es todo, amigos!")
